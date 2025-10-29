b5faf2855d4ca580f04fc3ae6c2f7c807584eb46
joao-vitorCC <joao.vitor2017cc@discente.ufg.br>
2025-10-29 20:55:12 -0300

    Add files via upload

---
+++ ast.c
@@ -0,0 +1,25 @@
+include<stdio.h>
+enum tipos{
+	programa,
+	declFuncV,
+	declP
+};
+struct no{
+	enum tipoNo t;
+	struct no f1;
+	struct no f2;
+	struct no f3;
+	union c td;
+};
+union c{
+	
+
+};
+int main(){
+
+
+
+
+
+return 0; 
+}
