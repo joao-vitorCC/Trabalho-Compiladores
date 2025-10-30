ddba6e8f308c0f506a4d707d0464bf93b3a9bd11
joao-vitorCC <joao.vitor2017cc@discente.ufg.br>
2025-10-30 12:52:29 -0300

    Definindo os atributos da AST

--- ast.c
+++ ast.c
@@ -1,25 +1,28 @@
-include<stdio.h>
-enum tipos{
+#include<stdio.h>
+#include<string.h>
+enum tiposNo{
 	programa,
 	declFuncV,
 	declP
 };
 struct no{
-	enum tipoNo t;
-	struct no f1;
-	struct no f2;
-	struct no f3;
-	union c td;
-};
-union c{
+	enum tiposNo t;
+	char nome[10000]; 
+	int valor; 
+	char *cadeia;
+	struct no *f1;
+	struct no* f2;
+	struct no *f3;
+	struct no * proximo;
 	
-
 };
+
 int main(){
-
-
-
-
+	struct no n1;
+	n1.t = programa;
+	strcpy(n1.nome,"ID1");
+	n1.valor = 10;
+	printf("%d %s %d",n1.t,n1.nome,n1.valor);
 
 return 0; 
 }
