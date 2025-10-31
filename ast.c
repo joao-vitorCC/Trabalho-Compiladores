735628f61ed12aa8fb3a1aba453e1225e4813581
joao-vitorCC <joao.vitor2017cc@discente.ufg.br>
2025-10-31 13:44:12 -0300

    criando o construtor

--- ast.c
+++ ast.c
@@ -1,5 +1,6 @@
 #include<stdio.h>
 #include<string.h>
+#include<stdlib.h>
 enum tiposNo{
 	programa,
 	declFuncV,
@@ -8,21 +9,31 @@ enum tiposNo{
 struct no{
 	enum tiposNo t;
 	char nome[10000]; 
-	int valor; 
-	char *cadeia;
+	char valor[10000]; 
 	struct no *f1;
 	struct no* f2;
 	struct no *f3;
 	struct no * proximo;
 	
 };
-
+struct no * criaNo(enum tiposNo t, char nome[],char valor[]){
+	struct no * novo;
+	novo = (struct no *)malloc(sizeof(struct no));
+	novo->t = t;
+	strcpy(novo->nome,nome);
+	strcpy(novo->valor,valor);
+	novo->f1 = NULL;
+	novo->f2 = NULL;
+	novo->f3 = NULL;
+	novo->proximo = NULL;
+return novo;	
+}
 int main(){
-	struct no n1;
-	n1.t = programa;
-	strcpy(n1.nome,"ID1");
-	n1.valor = 10;
-	printf("%d %s %d",n1.t,n1.nome,n1.valor);
+	struct no * n1;
+	enum tiposNo t1 = programa;
+	
+	n1 = criaNo(t1,"ID1","10");
+	printf("%d %s %s",n1->t,n1->nome,n1->valor);
 
 return 0; 
 }
