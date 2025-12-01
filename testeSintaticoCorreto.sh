1de0b0342388b8f011ddcaa2fa06266279121318
joao-vitorCC <joao.vitor2017cc@discente.ufg.br>
2025-12-01 10:58:22 -0300

    shell para testar sintatico

---
+++ testeSintaticoCorreto.sh
@@ -0,0 +1,45 @@
+#!/bin/bash
+	echo -e "\\n-----------------------"
+	echo "-----------------------"
+	echo "-----------------------"
+    echo "Teste fatorialCorreto.g"
+    echo "-----------------------"
+    echo "-----------------------"
+    echo -e "-----------------------\\n"
+    ./goianinha fatorialCorreto.g
+
+	echo -e "\\n-----------------------"
+	echo "-----------------------"
+	echo "-----------------------"
+	echo "Teste FibEfatCorreto.g"
+	echo "-----------------------"
+	echo "-----------------------"
+	echo -e "-----------------------\\n"
+    ./goianinha FibEfatCorreto.g
+    
+    echo -e "\\n-----------------------"
+    echo "-----------------------"
+    echo "-----------------------"
+    echo "Teste FibEfatCorretoVersao2.g"
+    echo "-----------------------"
+    echo "-----------------------"
+    echo -e "-----------------------\\n"
+    ./goianinha FibEfatCorretoVersao2.g
+    
+    echo -e "\\n-----------------------"
+    echo "-----------------------"
+    echo "-----------------------"
+    echo "Teste NotaEmConceito.g"
+    echo "-----------------------"
+    echo "-----------------------"
+    echo -e "-----------------------\\n"
+    ./goianinha NotaEmConceito.g
+    
+    echo -e "\\n-----------------------"
+    echo "-----------------------"
+    echo "-----------------------"
+    echo "Teste SeqOrdenada.g"
+    echo "-----------------------"
+    echo "-----------------------"
+    echo -e "-----------------------\\n"
+    ./goianinha SeqOrdenada.g
